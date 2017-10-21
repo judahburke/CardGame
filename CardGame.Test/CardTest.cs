51344085ad489d2b5cd2b6dcacc9aa03144f13b4
Judah Burke <judahburke@yahoo.com>
2017-10-21 17:34:42 -0400

    Added fundamental solution

---
+++ CardGame.Test/CardTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardGame.Test
+{
+    [TestClass]
+    public class CardTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ CardGame.Test/DealerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardGame.Test
+{
+    [TestClass]
+    public class DealerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
